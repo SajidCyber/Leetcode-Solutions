dc2ecc11bd4ff0f78fa56fb7ff4bc7c99b9c977a
Sajid Mansuri <actorsajid132@gmail.com>
2026-07-17 18:16:12 +0530

    Time: 3 ms (82.98%), Space: 20.9 MB (70.7%) - LeetHub

---
+++ 2363-merge-similar-items/2363-merge-similar-items.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    vector<vector<int>> mergeSimilarItems(vector<vector<int>>& items1, vector<vector<int>>& items2) {
+        map<int, int> m;
+        
+        for (const auto& item : items1) {
+            m[item[0]] += item[1];
+        }
+        
+        for (const auto& item : items2) {
+            m[item[0]] += item[1];
+        }
+        
+        vector<vector<int>> ret;
+        for (const auto& [value, weight] : m) {
+            ret.push_back({value, weight});
+        }
+        
+        return ret;
+    }
+};
